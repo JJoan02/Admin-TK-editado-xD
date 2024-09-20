230d3751c9fba6ebfa6c0285622d1e45cd042108
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-09-20 03:34:25 -0500

    Create connection.js

---
+++ src/core/connection.js
@@ -0,0 +1,21 @@
+import { makeWASocket } from '../lib/simple.js';
+import { state, saveCreds } from './auth.js'; // Autenticación
+import pino from 'pino';
+
+export async function startConnection() {
+  const conn = makeWASocket({
+    logger: pino({ level: 'silent' }),
+    auth: {
+      creds: state.creds,
+      keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' })),
+    }
+  });
+
+  conn.ev.on('connection.update', update => {
+    if (update.connection === 'open') {
+      console.log('🟢 Conexión establecida');
+    }
+  });
+
+  conn.ev.on('creds.update', saveCreds);
+}
