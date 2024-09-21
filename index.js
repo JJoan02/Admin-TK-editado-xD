916c8eb92ad973d0b2dfa5f85ffaac47b1a9bf24
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-09-21 10:35:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import { displayHeader, start } from './src/core/init.js'; // Importar 'displayHeader' y 'start'
+import { displayHeader, start } from './src/core/init.js';
 import chalk from 'chalk';
 
 console.log(chalk.green('✰ Iniciando Admin-TK ✰'));
 
-// Mostrar el encabezado y luego iniciar el bot
 displayHeader();
-start('../../start.js');  // Asegúrate de pasar el archivo correcto para iniciar
+start('./start.js');
