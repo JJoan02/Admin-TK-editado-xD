af0f0b6177ac040329528a0c0bf74b1e4fea2c3e
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-09-21 10:50:25 -0500

    Delete src/core/start.js

--- src/core/start.js
+++
@@ -1,8 +0,0 @@
-import { displayHeader, start } from './src/core/init.js';
-import chalk from 'chalk';
-
-console.log(chalk.green('✰ Iniciando Admin-TK ✰'));
-
-// Mostrar el encabezado y luego iniciar el bot
-displayHeader();
-start('./start.js');  // Ruta corregida sin saltos de línea
